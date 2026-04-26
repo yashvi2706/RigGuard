f752ad7d049a32582737c6b374dfa442a74e8d2d
Yashvi <yashvi270607@gmail.com>
2026-04-26 15:45:07 +0530

    Add cleanup and reset functions for semaphores

--- resources.h
+++ resources.h
@@ -5,6 +5,8 @@
 #include "filestore.h"
 
 void init_semaphores();
+void cleanup_semaphores();
+void reset_semaphores();
 void request_resource(const char *username, const char *role);
 void release_resource(const char *username);
 void view_allocation_table();
