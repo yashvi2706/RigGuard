eadcea970390d2be2741acfd7c42e0b13c2d758d
Yashvi <yashvi270607@gmail.com>
2026-04-26 15:46:15 +0530

    Refactor resource_sem and add num_resources
    Changed resource_sem to a pointer array and added num_resources variable.

--- utils.c
+++ utils.c
@@ -2,7 +2,8 @@
 
 pthread_mutex_t resource_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t file_mutex     = PTHREAD_MUTEX_INITIALIZER;
-sem_t           resource_sem[NUM_RESOURCES];
+sem_t *resource_sem[MAX_RESOURCES];
+int    num_resources = 0;
 
 void get_timestamp(char *buf, int size) {
     time_t now = time(NULL);
@@ -23,9 +24,7 @@ void print_separator() {
     printf(CYAN "──────────────────────────────────────────────────\n" RESET);
 }
 
-void clear_screen() {
-    printf("\033[2J\033[H");
-}
+void clear_screen() { printf("\033[2J\033[H"); }
 
 void color_print(const char *color, const char *msg) {
     printf("%s%s%s\n", color, msg, RESET);
