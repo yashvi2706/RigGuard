8c868ac03716f3043c5b20756c9b96e70fd4e43f
Yashvi <yashvi270607@gmail.com>
2026-04-26 15:43:50 +0530

    Refactor filestore.h to add new operations

--- filestore.h
+++ filestore.h
@@ -3,21 +3,19 @@
 
 #include "utils.h"
 
-// User file operations
-void     init_data_files();
-int      load_users(User users[], int *count);
-void     save_users(User users[], int count);
+void init_data_files();
+int  load_users(User users[], int *count);
+void save_users(User users[], int count);
+int  load_resources(Resource res[], int *count);
+void save_resources(Resource res[], int count);
+void append_message(const char *sender, const char *receiver, const char *msg);
+void print_messages(const char *for_user);
+void log_incident(const char *actor, const char *action, const char *detail);
+void print_incidents();
 
-// Resource file operations
-int      load_resources(Resource res[], int *count);
-void     save_resources(Resource res[], int count);
-
-// Message operations
-void     append_message(const char *sender, const char *receiver, const char *msg);
-void     print_messages(const char *for_user);
-
-// Incident log
-void     log_incident(const char *actor, const char *action, const char *detail);
-void     print_incidents();
+// New: registration and equipment management
+void register_crew(const char *admin_role);
+void add_equipment(const char *admin_role);
+void unlock_crew(const char *admin_role);
 
 #endif
